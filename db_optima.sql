834966c461278fbe8eb19e37f19882841cba46c7
marcialaldair <marcialaldair@gmail.com>
2022-12-15 19:30:11 -0600

    otro commit

---
+++ db_optima.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 15, 2022 at 01:12 PM
+-- Server version: 8.0.31-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_optima`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Clientes`
+--
+
+CREATE TABLE `Clientes` (
+  `ID` int NOT NULL,
+  `Name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Years` int NOT NULL,
+  `Telephone_Number` int NOT NULL,
+  `Email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Car` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Car_model` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Modelos`
+--
+
+CREATE TABLE `Modelos` (
+  `Marca` tinytext NOT NULL,
+  `modelo` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Modelos`
+--
+
+INSERT INTO `Modelos` (`Marca`, `modelo`) VALUES
+('Honda', 'CRV'),
+('Honda', 'HRV'),
+('Honda', 'BRV'),
+('KIA', 'SOUL'),
+('KIA', 'RIO'),
+('KIA', 'SPORTAGE'),
+('Ford', 'MUSTANG'),
+('Ford', 'ESCAPE'),
+('Ford', 'FIESTA'),
+('Nissan', 'VERSA'),
+('Nissan', 'MARCH'),
+('Nissan', 'SENTRA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Clientes`
+--
+ALTER TABLE `Clientes`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Clientes`
+--
+ALTER TABLE `Clientes`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
